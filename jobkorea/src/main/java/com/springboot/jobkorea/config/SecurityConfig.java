eefe384b0bc123637e10a1915e85154bea2ba290
john <deadpoetspoet@naver.com>
2022-01-17 15:31:22 +0900

    Revert "2022-01-17 15:26"
    This reverts commit 8d4d32c64ce418d4f69eb6bbf3e17d528c88db1e.

--- jobkorea/src/main/java/com/springboot/jobkorea/config/SecurityConfig.java
+++ jobkorea/src/main/java/com/springboot/jobkorea/config/SecurityConfig.java
@@ -31,7 +31,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.formLogin()
 			.loginPage("/auth/signin")
 			.loginProcessingUrl("/auth/signin")
-		
+			.loginPage("/auth/signinCopmp")
+			.loginProcessingUrl("/auth/signinCopmp")
 			.defaultSuccessUrl("/");
 		
 	}	
